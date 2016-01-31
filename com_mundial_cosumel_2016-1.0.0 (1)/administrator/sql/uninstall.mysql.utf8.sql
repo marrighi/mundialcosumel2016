0b931d6e4af33b1429dc6649163fbbc1ac3f2570
mervidela <mervidela@hotmail.com>
2016-01-30 22:17:47 -0300

    como quedo en tyr

--- com_mundial_cosumel_2016-1.0.0 (1)/administrator/sql/uninstall.mysql.utf8.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__mc2016_inscripcion`;
-DROP TABLE IF EXISTS `#__mc2016_gender`;
-DROP TABLE IF EXISTS `#__mc2016_tshirt`;
-DROP TABLE IF EXISTS `#__mc2016_blood_type`;
-DROP TABLE IF EXISTS `#__mc2016_event`;
